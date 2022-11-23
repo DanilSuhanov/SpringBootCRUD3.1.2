5561359bae81a32895ea2162d208ea9f98376df2
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2022-11-23 19:10:27 +0300

    done

--- src/main/java/ru/ant/springbootcrud/controller/UserController.java
+++ src/main/java/ru/ant/springbootcrud/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.ant.springbootcrud.model.User;
 import ru.ant.springbootcrud.service.UserService;
 
 @Controller
@@ -16,5 +19,35 @@ public class UserController {
     public String index(Model model) {
         model.addAttribute("users", userService.getAllUser());
         return "index";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping("/new")
+    public String createUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(User user) {
+        userService.saveUser(user);
+        return "redirect:/";
     }
 }
--- src/main/java/ru/ant/springbootcrud/repository/UserRepository.java
+++ src/main/java/ru/ant/springbootcrud/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package ru.ant.springbootcrud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.ant.springbootcrud.model.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
