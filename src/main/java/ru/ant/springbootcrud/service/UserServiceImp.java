e1843c9ad7964f63ee4e50d7e55ed01580d91802
Данил <81508158+DanilSuhanov@users.noreply.github.com>
2022-11-26 12:07:03 +0300

    Transactional

--- src/main/java/ru/ant/springbootcrud/service/UserServiceImp.java
+++ src/main/java/ru/ant/springbootcrud/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package ru.ant.springbootcrud.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ant.springbootcrud.model.User;
 import ru.ant.springbootcrud.repository.UserRepository;
 
@@ -18,21 +19,25 @@ public class UserServiceImp implements  UserService {
     }
 
     @Override
+    @Transactional
     public User getUser(Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public List<User> getAllUser() {
         return userRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
